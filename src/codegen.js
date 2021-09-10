372d2ded5d88981af22c7dd93b0c9077029bfe94
Teemu Taskula <themetor@gmail.com>
2021-09-10 11:28:04 +0300

    Cleanup codegen

--- src/codegen.js
+++ src/codegen.js
@@ -43,58 +43,51 @@ export default class Codegen {
     return camelCase(name);
   }
 
-  handleTokens(name, values, temp) {
-    const config = {
-      ...DEFAULT_CONFIG,
-      ...this.config.codegen.defaults,
-      ...(this.config.codegen[name] || {}),
-    };
+  write() {
+    Object.entries(this.tokens).map(([name, { values }]) => {
+      const config = {
+        ...DEFAULT_CONFIG,
+        ...this.config.codegen.defaults,
+        ...(this.config.codegen[name] || {}),
+      };
 
-    const filename = config.filename || name;
-    const tokenCase = config.tokenCase || "camel";
+      const filename = config.filename || name;
+      const tokenCase = config.tokenCase || "camel";
 
-    const tokens = Object.entries(values)
-      .map(([k, v]) => [this.formatTokenName(k, tokenCase), v])
-      .filter(this.filterTokens)
-      .sort(this.sortTokens);
+      const tokens = Object.entries(values)
+        .map(([k, v]) => [this.formatTokenName(k, tokenCase), v])
+        .filter(this.filterTokens)
+        .sort(this.sortTokens);
 
-    if (config.type === "ts" || config.type === "js") {
-      const compiled = template(temp);
-      const filetype = config.type;
+      if (config.type === "ts" || config.type === "js") {
+        const compiled = template(TEMPLATE);
+        const filetype = config.type;
 
-      fs.writeFileSync(`tokens/${filename}.${filetype}`, compiled({ tokens }));
-    }
-
-    if (config.type === "json") {
-      const json = tokens.reduce((acc, val) => {
-        acc[val[0]] = val[1];
-        return acc;
-      }, {});
-
-      fs.writeFileSync(`tokens/${filename}`, JSON.stringify(json, null, 2));
-    }
-
-    if (config.type === "svg") {
-      const dirname = `tokens/${config.dirname || name}`;
-
-      if (!fs.existsSync(dirname)) {
-        fs.mkdirSync(dirname);
+        fs.writeFileSync(
+          `tokens/${filename}.${filetype}`,
+          compiled({ tokens })
+        );
       }
 
-      tokens.forEach((token) => {
-        fs.writeFileSync(`${dirname}/${token[0]}.svg`, token[1]);
-      });
-    }
-  }
+      if (config.type === "json") {
+        const json = tokens.reduce((acc, val) => {
+          acc[val[0]] = val[1];
+          return acc;
+        }, {});
 
-  write() {
-    Object.entries(this.tokens).map(([name, { type, values }]) => {
-      if (type === "color" || type === "svg") {
-        this.handleTokens(name, values, SIMPLE_VALUE_TEMPLATE);
-      } else if (type === "height" || type === "width" || type === "radius") {
-        this.handleTokens(name, values, SIMPLE_VALUE_TEMPLATE);
-      } else {
-        this.handleTokens(name, values, OBJECT_TEMPLATE);
+        fs.writeFileSync(`tokens/${filename}`, JSON.stringify(json, null, 2));
+      }
+
+      if (config.type === "svg") {
+        const dirname = `tokens/${config.dirname || name}`;
+
+        if (!fs.existsSync(dirname)) {
+          fs.mkdirSync(dirname);
+        }
+
+        tokens.forEach((token) => {
+          fs.writeFileSync(`${dirname}/${token[0]}.svg`, token[1]);
+        });
       }
     });
   }
@@ -104,12 +97,8 @@ const DEFAULT_CONFIG = {
   type: "ts",
 };
 
-const SIMPLE_VALUE_TEMPLATE =
-  "<% tokens.forEach(function(token) { %>" +
-  "export const <%= token[0] %> = '<%= token[1] %>';\n" +
-  "<% }); %>";
-
-const OBJECT_TEMPLATE =
+const TEMPLATE =
+  "/* eslint-disable */\n" +
   "<% tokens.forEach(function(token) { %>" +
   "export const <%= token[0] %> = <%= JSON.stringify(token[1], null, 2) %>;\n" +
   "<% }); %>";
